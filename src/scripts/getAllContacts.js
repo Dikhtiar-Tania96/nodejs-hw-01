04dcf4a3b1151b6c0a26922a4426caa4a3d71ba0
Dikhtiar Tania <you@example.com>
2024-08-06 14:48:48 +0300

    push task 4

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises'
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+    try{
+        const data = await fs.readFile(PATH_DB, "UTF-8");
+        return JSON.parse(data);
+    } catch (error){
+        console.error('Error: Помилка виконання файлу', error);
+    }
+};
 
 console.log(await getAllContacts());
