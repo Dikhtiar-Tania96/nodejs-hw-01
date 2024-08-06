4b8bc0d62a89733e14d6ba5f60ea6d35bcdc6077
Dikhtiar Tania <you@example.com>
2024-08-06 14:45:18 +0300

    push tsk 3

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,16 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
 export const addOneContact = async () => {};
-
+try{
+    const data = await fs.readFile(PATH_DB, 'UTF-8');
+    const contacts = JSON.parse(data);
+    // додає до масиву лише один згенерований контакт
+    contacts.push(createFakeContact());
+    //збереження змін у файлі
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+} catch(error){
+    console.error('Error: Помилка',error);
+}
 addOneContact();
